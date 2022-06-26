8b758937ab29a27e73c8bb576b07f747045f171e
Bekzhan <bekjando@gmail.com>
2022-06-26 14:26:24 +0300

    Initial commit

---
+++ src/test/java/com/bekjando/orderBook/OrderBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bekjando.orderBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
