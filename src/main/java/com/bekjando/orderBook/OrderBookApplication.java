cae92d029a4d133c73b743800d668e8920a90682
Bekzhan <bekjando@gmail.com>
2022-07-15 00:02:29 +0300

    вот так пока работает без ошибок, но если в мейн классе убрать закомментированное, выдает ошибки: org.springframework.dao.IncorrectResultSizeDataAccessException: query did not return a unique

--- src/main/java/com/bekjando/orderBook/OrderBookApplication.java
+++ src/main/java/com/bekjando/orderBook/OrderBookApplication.java
@@ -6,6 +6,7 @@ import com.bekjando.orderBook.service.CustomerService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import java.util.ArrayList;
 
@@ -16,35 +17,37 @@ public class OrderBookApplication {
 		SpringApplication.run(OrderBookApplication.class, args);
 	}
 
-	CommandLineRunner run(CustomerService customerService){
-		return args -> {
-			customerService.saveRole(new Role(null, "ROLE_CLIENT"));
-			customerService.saveRole(new Role(null, "ROLE_MANAGER"));
-			customerService.saveRole(new Role(null, "ROLE_ADMIN"));
-			customerService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
 
-			customerService.saveCustomer(new Customer
-					(null, "Fernando Torres", "elnino",
-					"fernando9torres", new ArrayList<>()));
-			customerService.saveCustomer(new Customer
-					(null, "Steven Gerrard", "stevege",
-							"steve8", new ArrayList<>()));
-			customerService.saveCustomer(new Customer
-					(null, "Jamie Carragher", "jamiec",
-							"jamie233223", new ArrayList<>()));
-			customerService.saveCustomer(new Customer
-					(null, "Dirk Kuyt", "rizhiy",
-							"superKuyt18", new ArrayList<>()));
-
-
-			customerService.addRoleToCustomer("elnino", "ROLE_CLIENT");
-			customerService.addRoleToCustomer("rizhiy", "ROLE_MANAGER");
-			customerService.addRoleToCustomer("jamiec", "ROLE_ADMIN");
-			customerService.addRoleToCustomer("stevege", "ROLE_CLIENT");
-			customerService.addRoleToCustomer("stevege", "ROLE_ADMIN");
-			customerService.addRoleToCustomer("stevege", "ROLE_SUPER_ADMIN");
-
-		};
-	}
+//	@Bean
+//	CommandLineRunner run(CustomerService customerService){
+//		return args -> {
+//			customerService.saveRole(new Role(null, "ROLE_CLIENT"));
+//			customerService.saveRole(new Role(null, "ROLE_MANAGER"));
+//			customerService.saveRole(new Role(null, "ROLE_ADMIN"));
+//			customerService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+//
+//			customerService.saveCustomer(new Customer
+//					(null, "Fernando Torres", "elnino",
+//					"fernando9torres", new ArrayList<>()));
+//			customerService.saveCustomer(new Customer
+//					(null, "Steven Gerrard", "stevege",
+//							"steve8", new ArrayList<>()));
+//			customerService.saveCustomer(new Customer
+//					(null, "Jamie Carragher", "jamiec",
+//							"jamie233223", new ArrayList<>()));
+//			customerService.saveCustomer(new Customer
+//					(null, "Dirk Kuyt", "rizhiy",
+//							"superKuyt18", new ArrayList<>()));
+//
+//
+//			customerService.addRoleToCustomer("elnino", "ROLE_CLIENT");
+//			customerService.addRoleToCustomer("rizhiy", "ROLE_MANAGER");
+//			customerService.addRoleToCustomer("jamiec", "ROLE_ADMIN");
+//			customerService.addRoleToCustomer("stevege", "ROLE_CLIENT");
+//			customerService.addRoleToCustomer("stevege", "ROLE_ADMIN");
+//			customerService.addRoleToCustomer("stevege", "ROLE_SUPER_ADMIN");
+//
+//		};
+//	}
 
 }
