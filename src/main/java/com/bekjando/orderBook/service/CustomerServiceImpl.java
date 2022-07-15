02b698daf0b583d8b96b3b62e22ad92554619509
Bekzhan <bekjando@gmail.com>
2022-07-15 18:36:56 +0300

    новый класс SecurityConfig, новый метод encoder() в мейнклассе, новый переопределённый метод loadByUsername() в Сервис пока выдает ошибку NoSuchBeanDefinitionException

--- src/main/java/com/bekjando/orderBook/service/CustomerServiceImpl.java
+++ src/main/java/com/bekjando/orderBook/service/CustomerServiceImpl.java
@@ -7,20 +7,43 @@ import com.bekjando.orderBook.repository.CustomerRepository;
 import com.bekjando.orderBook.repository.RoleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 @Slf4j
-public class CustomerServiceImpl implements CustomerService{
+public class CustomerServiceImpl implements CustomerService, UserDetailsService {
 
     private final CustomerRepository customerRepository;
 
     private final RoleRepository roleRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Customer customer = customerRepository.findByUserName(username);
+        if(customer == null){
+            log.error("Данного пользователя нет в Базе Данных");
+            throw new UsernameNotFoundException("Данного пользователя нет в Базе Данных");
+        } else {
+            log.info("Данный пользователь найден в Базе Данных {}", username);
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        customer.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return new User(customer.getUserName(), customer.getPassword(), authorities);
+    }
 
     @Override
     public Customer saveCustomer(Customer customer) {
@@ -53,4 +76,6 @@ public class CustomerServiceImpl implements CustomerService{
         log.info("Fetching all users");
         return customerRepository.findAll();
     }
+
+
 }
